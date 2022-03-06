56d1aa24c895af4f91d2090d39e957dda23da294
Pier Fumagalli <pier@usrz.com>
2022-03-06 23:08:38 +0100

    Setup to TypeScript

--- test/setup.ts
+++ test/setup.ts
@@ -1,12 +1,12 @@
-'use strict'
-
 console.log(`Running on NodeJS version ${process.version}`)
 process.on('exit', () => console.log(`Running on NodeJS version ${process.version}`))
 
 /* Always use "chai as promised" */
-require('chai').use(require('chai-as-promised'))
+import chai from 'chai'
+import chap from 'chai-as-promised'
+import chae from 'chai-exclude'
+chai.use(chap).use(chae)
 
 /* Support source maps as we mangle code for coverage */
-require('source-map-support').install({
-  hookRequire: true,
-})
+import sms from 'source-map-support'
+sms.install({ hookRequire: true })
