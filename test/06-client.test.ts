f8951cce800c2da008207b084745dec3e19e7109
Pier Fumagalli <pier@usrz.com>
2022-03-10 22:57:45 +0100

    Client test

---
+++ test/06-client.test.ts
@@ -0,0 +1,335 @@
+import { AssertionError } from 'assert'
+import { expect } from 'chai'
+import { randomBytes } from 'crypto'
+import { Adapter, Client, ClusterAdapter, ServerAdapter } from '../src/index'
+
+describe('Memcached Client', () => {
+  const host = process.env.MEMCACHED_HOST || '127.0.0.1'
+  const port = parseInt(process.env.MEMCACHED_PORT || '11211')
+
+  const client = new Client({ hosts: [ { host, port } ] })
+
+  let key: string
+
+  beforeEach(() => key = randomBytes(30).toString('base64'))
+
+  describe('construction', () => {
+    it('should construct without any arguments', () => {
+      const _hosts = process.env.MEMCACHED_HOSTS
+      const _ttl = process.env.MEMCACHED_TTL
+      const _timeout = process.env.MEMCACHED_TIMEOUT
+      try {
+        process.env.MEMCACHED_HOSTS = 'host1'
+        delete process.env.MEMCACHED_TTL
+        delete process.env.MEMCACHED_TIMEOUT
+
+        const client = new Client()
+        expect(client.adapter).to.be.instanceof(ClusterAdapter)
+
+        const adapter = client.adapter as ClusterAdapter
+        expect(adapter.servers).to.be.an('array')
+        expect(adapter.servers[0].host).to.equal('host1')
+      } finally {
+        process.env.MEMCACHED_HOSTS = _hosts
+        process.env.MEMCACHED_TTL = _ttl
+        process.env.MEMCACHED_TIMEOUT = _timeout
+      }
+    })
+
+    it('should construct with some options', () => {
+      const client = new Client({ hosts: 'host1' })
+      expect(client.adapter).to.be.instanceof(ClusterAdapter)
+
+      const adapter = client.adapter as ClusterAdapter
+      expect(adapter.servers).to.be.an('array')
+      expect(adapter.servers[0].host).to.equal('host1')
+    })
+
+    it('should construct with an adapter', () => {
+      const server = new ServerAdapter({ host: 'host1' })
+      const client = new Client(server)
+      expect(client.adapter).to.equal(server)
+    })
+
+    it('should not construct with something else', () => {
+      expect(() => new Client({} as any))
+          .to.throw(AssertionError, 'Invalid client constructor arguments')
+    })
+  })
+
+  describe('basic data types', () => {
+    const tests = {
+      'bigint': 123n,
+      'string': 'FooBar!',
+      'number': 54321.98,
+      'true (boolean)': true,
+      'false (boolean)': false,
+      'object': { n: 1, b: false },
+      'array': [ 1, 'foobar', true ],
+      'null': null,
+    } as const
+
+    for (const [ test, value ] of Object.entries(tests)) {
+      it(test, async () => {
+        const cas = await client.set(key, value)
+        expect(await client.get(key)).eql({ value, cas })
+      })
+    }
+
+    it('undefined (fail)', async () => {
+      await expect(client.set(key, <any> undefined))
+          .to.be.rejectedWith(AssertionError, 'Unable to store value of type "undefined"')
+    })
+
+    it('symbol (fail)', async () => {
+      await expect(client.set(key, <any> Symbol()))
+          .to.be.rejectedWith(AssertionError, 'Unable to store value of type "symbol"')
+    })
+  })
+
+  describe('buffers and typed arrays', () => {
+    const buffer = randomBytes(128)
+
+    const tests = {
+      Uint8Array: Uint8Array,
+      Uint8ClampedArray: Uint8ClampedArray,
+      Uint16Array: Uint16Array,
+      Uint32Array: Uint32Array,
+      Int8Array: Int8Array,
+      Int16Array: Int16Array,
+      Int32Array: Int32Array,
+      BigUint64Array: BigUint64Array,
+      BigInt64Array: BigInt64Array,
+      Float32Array: Float32Array,
+      Float64Array: Float64Array,
+    } as const
+
+    it('Buffer', async () => {
+      const set = await client.set(key, buffer)
+      const { value, cas } = await client.get(key) || {}
+      expect(cas).to.equal(set)
+      expect(Buffer.compare(value as any, buffer)).to.equal(0)
+    })
+
+    for (const [ test, TypedArray ] of Object.entries(tests)) {
+      it(test, async () => {
+        const array = new TypedArray(buffer.buffer, buffer.byteOffset, buffer.byteLength / 8)
+        const set = await client.set(key, array)
+        const { value, cas } = await client.get(key) || {} as any
+        expect(cas).to.equal(set)
+        expect(value).to.be.instanceOf(TypedArray)
+        expect(value.length).to.equal(array.length).to.be.greaterThan(0)
+        expect([ ...value ]).to.have.members([ ...array ]).to.have.length.greaterThan(0)
+      })
+    }
+  })
+
+  /* ======================================================================== */
+
+  describe('add/replace', () => {
+    it('should add a value when none exists', async () => {
+      const cas = await client.add(key, 'Foo, Bar and Baz')
+      expect(cas).to.be.a('bigint')
+
+      const get = await client.get(key)
+      expect(get).to.eql({
+        value: 'Foo, Bar and Baz',
+        cas,
+      })
+
+      expect(await client.add(key, 'Hello, world!')).to.be.undefined
+
+      expect(await client.get(key)).to.eql(get)
+    })
+
+    it('should replace a value when none exists', async () => {
+      expect(await client.replace(key, 'Foo, Bar and Baz')).to.be.undefined
+      expect(await client.get(key)).to.be.undefined
+
+      const cas = await client.set(key, 'Foo, Bar and Baz')
+      expect(cas).to.be.a('bigint')
+
+      expect(await client.get(key)).to.eql({
+        value: 'Foo, Bar and Baz',
+        cas,
+      })
+
+      const replace = await client.replace(key, 'Hello, world!')
+      expect(replace).to.be.a('bigint')
+
+      expect(await client.get(key)).to.eql({
+        value: 'Hello, world!',
+        cas: replace,
+      })
+    })
+  })
+
+  /* ======================================================================== */
+
+  describe('append/prepend', () => {
+    it('should append a value', async () => {
+      expect(await client.append(key, 'Hello, world!')).to.be.false
+
+      const cas = await client.set(key, 'Foo, Bar')
+      expect(cas).to.be.a('bigint')
+
+      expect(await client.get(key)).to.eql({
+        value: 'Foo, Bar',
+        cas,
+      })
+
+      expect(await client.append(key, ' and Baz')).to.be.true
+
+      const get = await client.get(key)
+      expect(get!.value).to.eql('Foo, Bar and Baz')
+    })
+
+    it('should prepend a value', async () => {
+      expect(await client.prepend(key, 'Hello, world!')).to.be.false
+
+      const cas = await client.set(key, 'Bar and Baz')
+      expect(cas).to.be.a('bigint')
+
+      expect(await client.get(key)).to.eql({
+        value: 'Bar and Baz',
+        cas,
+      })
+
+      expect(await client.prepend(key, 'Foo, ')).to.be.true
+
+      const get = await client.get(key)
+      expect(get!.value).to.eql('Foo, Bar and Baz')
+    })
+  })
+
+  /* ======================================================================== */
+
+  describe('increment/decrement', () => {
+    it('should increment a value', async () => {
+      const { cas, value } = await client.increment(key, 1, { initial: 123 }) as any
+
+      expect(cas).to.be.a('bigint')
+      expect(value).to.be.a('bigint')
+      expect(value).to.equal(123n)
+
+      expect(await client.get(key)).to.eql({
+        value: 123n,
+        cas,
+      })
+
+      const { cas: cas2 } = await client.increment(key, 1) as any
+      expect(await client.get(key)).to.eql({
+        value: 124n,
+        cas: cas2,
+      })
+    })
+
+    it('should increment a value modified by a race condition', async () => {
+      process.nextTick(() => client.set(key, 'foobar'))
+      const promise = client.increment(key, 1, { initial: 123 }) as any
+
+      const { cas, value } = await promise
+
+      expect(cas).to.be.a('bigint')
+      expect(value).to.be.a('bigint')
+      expect(value).to.equal(123n)
+
+      const result = await client.get(key)
+      expect(result?.value).to.equal('foobar')
+      expect(result?.cas).to.not.equal(cas)
+    })
+
+    it('should decrement a value', async () => {
+      const { cas, value } = await client.decrement(key, 1, { initial: 123 }) as any
+      expect(cas).to.be.a('bigint')
+
+      expect(value).to.be.a('bigint')
+      expect(value).to.equal(123n)
+
+      expect(await client.get(key)).to.eql({
+        value: 123n,
+        cas,
+      })
+
+      const { cas: cas2 } = await client.decrement(key, 1) as any
+      expect(await client.get(key)).to.eql({
+        value: 122n,
+        cas: cas2,
+      })
+    })
+
+    it('should decrement a value modified by a race condition', async () => {
+      process.nextTick(() => client.set(key, 'foobar'))
+      const promise = client.decrement(key, 1, { initial: 123 }) as any
+
+      const { cas, value } = await promise
+
+      expect(cas).to.be.a('bigint')
+      expect(value).to.be.a('bigint')
+      expect(value).to.equal(123n)
+
+      const result = await client.get(key)
+      expect(result?.value).to.equal('foobar')
+      expect(result?.cas).to.not.equal(cas)
+    })
+  })
+
+  /* ======================================================================== */
+
+  describe('adapter passthrough', () => {
+    const client = new Client({
+      get(...args: any[]): any {
+        throw new Error('Method not implemented:' + args)
+      },
+      touch(...args: any[]): any {
+        return [ 'touch', ...args ]
+      },
+      delete(...args: any[]): any {
+        return [ 'delete', ...args ]
+      },
+      flush(...args: any[]): any {
+        return [ 'flush', ...args ]
+      },
+      noop(...args: any[]): any {
+        return [ 'noop', ...args ]
+      },
+      quit(...args: any[]): any {
+        return [ 'quit', ...args ]
+      },
+      version(...args: any[]): any {
+        return [ 'version', ...args ]
+      },
+      stats(...args: any[]): any {
+        return [ 'stats', ...args ]
+      },
+    } as Adapter)
+
+    it('touch', () => {
+      expect(client.touch('key', { ttl: 123 })).to.eql([ 'touch', 'key', { ttl: 123 } ])
+    })
+
+    it('delete', () => {
+      expect(client.delete('key', { cas: 123n })).to.eql([ 'delete', 'key', { cas: 123n } ])
+    })
+
+    it('flush', () => {
+      expect(client.flush(123)).to.eql([ 'flush', 123 ])
+    })
+
+    it('noop', () => {
+      expect(client.noop()).to.eql([ 'noop' ])
+    })
+
+    it('quit', () => {
+      expect(client.quit()).to.eql([ 'quit' ])
+    })
+
+    it('version', () => {
+      expect(client.version()).to.eql([ 'version' ])
+    })
+
+    it('stats', () => {
+      expect(client.stats()).to.eql([ 'stats' ])
+    })
+  })
+})
