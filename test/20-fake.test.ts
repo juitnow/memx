afe5643f4d93b00719941e7c7699f75cb1ff3e44
Pier Fumagalli <pier@usrz.com>
2022-03-17 22:18:43 +0100

    Reuse server tests for fake adapter

---
+++ test/20-fake.test.ts
@@ -0,0 +1,29 @@
+import { expect } from 'chai'
+import { FakeAdapter } from '../src/index'
+import { adapterTests } from './adapter'
+
+describe('Fake Adapter', () => {
+  const adapter = new FakeAdapter()
+
+  adapterTests(adapter)
+
+  describe('noop/quit/version/stats', () => {
+    it('should issue a noop', async () => {
+      expect(await adapter.noop()).to.be.undefined
+    })
+
+    it('should quit', async () => {
+      expect(await adapter.quit()).to.be.undefined
+    })
+
+    it('should get the version', async () => {
+      const version = await adapter.version()
+      expect(version).to.eql({ fake: '0.0.0-fake' })
+    })
+
+    it('should get the stats', async () => {
+      const stats = await adapter.stats()
+      expect(stats).to.eql({ fake: { version: '0.0.0-fake' } })
+    })
+  })
+})
