57366fd67f6a54f34033a4d751d7bdad39570124
Pier Fumagalli <pier@usrz.com>
2022-03-17 21:22:14 +0100

    Some docs

--- src/buffers.ts
+++ src/buffers.ts
@@ -3,10 +3,19 @@ import { BUFFERS } from './constants'
 const pool: Buffer[] = new Array(BUFFERS.POOL_SIZE)
 let offset = -1
 
+/** A `RecyclableBuffer` is a {@link Buffer} that can be recycled by a pool. */
 export interface RecyclableBuffer extends Buffer {
+  /** Recycle this buffer. */
   recycle(): void
 }
 
+/**
+ * Allocate a {@link RecyclableBuffer} of _size_ bytes.
+ *
+ * Up to {@link BUFFERS.POOL_SIZE} (64) buffers will be recycled once their
+ * `recycle()` method is called _if_ their size is not bigger than
+ * {@link BUFFERS.BUFFER_SIZE} (8192 bytes).
+ */
 export function allocateBuffer(size: number): RecyclableBuffer {
   if (size > BUFFERS.BUFFER_SIZE) {
     const buffer = Buffer.allocUnsafeSlow(size) as RecyclableBuffer
