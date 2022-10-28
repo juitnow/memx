26cae79a74c9bb7c6f03716b6954b945097cbf30
Pier Fumagalli <pier@usrz.com>
2022-10-28 15:41:18 +0200

    Start/stop memcached for tests (I keep forgetting!)

---
+++ test/00-init.test.ts
@@ -0,0 +1,28 @@
+import { log } from '@plugjs/plug'
+import { spawn } from 'node:child_process'
+
+before(() => {
+  return new Promise<void>((resolve, reject) => {
+    log('Starting "memcached"')
+
+    const child = spawn('memcached', { stdio: 'inherit' })
+
+    const timeout = setTimeout(() => {
+      after(() => {
+        log('Terminating "memcached"')
+        child.kill()
+      })
+      resolve()
+    }, 1000)
+
+    child.on('error', (err) => {
+      clearTimeout(timeout)
+      reject(err)
+    })
+
+    child.on('exit', (code, signal) => {
+      clearTimeout(timeout)
+      return reject(new Error(`Memcached exited with ${signal || code}`))
+    })
+  })
+})
